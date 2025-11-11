01d644f26f350edd359a5598fbc02f57b527bf3c
Noam Benhamou <noam.benhamou@post.runi.ac.il>
2025-11-11 19:41:28 +0200

    noamb hw

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,27 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+     String word = args[0];
+    int repeatCount = Integer.parseInt(args[1]);
+    word = word.toUpperCase();
+    String specialLetters = "AEFHILMNORSX";
+
+    for (int i = 0; i < word.length(); i++) {
+        char letter = word.charAt(i);
+
+        if (specialLetters.indexOf(letter) != -1) {
+            System.out.println("Give me an " + letter + ": " + letter + "!");
+        } else {
+            System.out.println("Give me a " + letter + ": " + letter + "!");
         }
+    }
+
+    System.out.println("What does that spell?");
+
+    for (int i = 0; i < repeatCount; i++) {
+        System.out.println(word + "!!!");
+    }
 }
+}
+	   
+
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,26 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int addmin = Integer.parseInt( args[1]);
+       // System.out.println("hours" + hours);
+       // System.out.println("min" + minutes);
+       // System.out.println("add" + addmin);
+       int totmin = hours * 60 + minutes + addmin;
+       int newmin = totmin % 60;
+       int newhours = ((totmin / 60) % 24);
+      if (newhours < 10 && newmin < 10) {
+        System.out.println("0"+ newhours + ":" + "0" + newmin);
+        
+      }
+      else if(newhours < 10 && newmin >= 10){
+        System.out.println("0"+ newhours + ":" + newmin);
+      }
+       else if(newhours >= 10 && newmin < 10){
+        System.out.println( newhours + ":" + "0" + newmin);
+      }
+      else{
+         System.out.println( newhours + ":" + newmin);
+      }
     }
 }
